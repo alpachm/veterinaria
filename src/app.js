4e40b53742314a4783ae98920092f12f5b23a2f3
alpachm <alexpacheco407@gmail.com>
2023-05-12 18:50:13 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const hpp = require('hpp');
+const xss = require('xss-clean');
+
+const app = express();
+
+const limiter = rateLimit({
+  max: 10000,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP. Please try again in one hour',
+});
+
+app.use(helmet());
+app.use(express.json());
+app.use(hpp());
+app.use(xss());
+app.use(cors());
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+app.use('api/v1', limiter);
+
+module.exports = app;
